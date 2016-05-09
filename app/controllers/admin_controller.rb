242d02d34709568b5064cd44a73fbc78c9cfc652
Anel <anelbasic@gmail.com>
2016-05-09 07:05:27 +0200

    Add restricting access to admin actions

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,11 @@
+class AdminController < ApplicationController
+
+  before_action :require_admin
+
+  def require_admin
+    unless current_user && current_user.role == 'admin'
+      redirect_to root_path
+    end
+  end
+  
+end
