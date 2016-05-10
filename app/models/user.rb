f09cb17db0f11441f67c95bde5e06fc408eae62e
Anel <anelbasic@gmail.com>
2016-05-10 21:57:39 +0200

    Fix typo, User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 
   validates :first_name, :last_name, :email, presence: true
-  valldates :email, uniqueness: true
-  
+  validates :email, uniqueness: true
+
   has_one :cart
   has_secure_password
 
