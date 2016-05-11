f7e7369fd7a6f2864f9f9b57a5be30547d5aa42d
Anel <anelbasic@gmail.com>
2016-05-11 13:01:20 +0200

    Add seeds for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,36 @@ User.create(first_name: 'Angelia', last_name: 'Asbill', email: 'asb@abc.com', pa
 User.create(first_name: 'Mina', last_name: 'Syed', email: 'syd@abc.com', password: 'abc', password_confirmation: 'abc')
 User.create(first_name: 'Ardath', last_name: 'Wendler', email: 'wend@abc.com', password: 'abc', password_confirmation: 'abc')
 User.create(first_name: 'Beckie', last_name: 'Goff', email: 'gof@abc.com', password: 'abc', password_confirmation: 'abc')
+
+Brand.create(title: 'Converse')
+Brand.create(title: 'Nike')
+Brand.create(title: 'Adidas')
+
+Product.create(title: 'Chuck Taylor All Star II', brand_id: 1, price: 70, style: 'casual', 
+              description: 'The Converse Chuck Taylor All Star II retains the iconic Chuck Taylor All Star silhouette you know and love, 
+              but is built for more to better meet the demands of your “on the go” lifestyle',
+              photo_url: 'https://s3.eu-central-1.amazonaws.com/imagesrandom/home.png')
+Product.create(title: 'Chuck Taylor All Star II', brand_id: 1, price: 75, style: 'casual', 
+              description: 'The Converse Chuck Taylor All Star II retains the iconic Chuck Taylor All Star silhouette you know and love, 
+              but is built for more to better meet the demands of your “on the go” lifestyle',
+              photo_url: 'https://s3.eu-central-1.amazonaws.com/imagesrandom/151161C_standard_small.jpg')
+Product.create(title: 'Chuck Taylor All Star II', brand_id: 1, price: 75, style: 'casual', 
+              description: 'The Converse Chuck Taylor All Star II retains the iconic Chuck Taylor All Star silhouette you know and love, 
+              but is built for more to better meet the demands of your “on the go” lifestyle',
+              photo_url: 'https://s3.eu-central-1.amazonaws.com/imagesrandom/150146C_standard_small.png')
+
+Product.create(title: 'Chuck Taylor All Star II InfTdlr', brand_id: 1, price: 75, style: 'casual', 
+              description: 'The Converse Chuck Taylor All Star II retains the iconic Chuck Taylor All Star silhouette you know and love, 
+              but is built for more to better meet the demands of your “on the go” lifestyle',
+              photo_url: 'https://s3.eu-central-1.amazonaws.com/imagesrandom/750143C_shot1_small.jpg')
+
+# create_table "products", force: :cascade do |t|
+#   t.integer  "brand_id"
+#   t.string   "title"
+#   t.integer  "price"
+#   t.string   "style"
+#   t.datetime "created_at",  null: false
+#   t.datetime "updated_at",  null: false
+#   t.text     "description"
+#   t.text     "photo_url"
+# end
