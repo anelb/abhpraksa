421977675fa8a2622a7e6b534d7b1a1a74846a44
Anel <anelbasic@gmail.com>
2016-05-17 13:32:40 +0200

    Add permisions to cart_item controller

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,4 +4,11 @@ class CartItemsController < ApplicationController
     
   end
 
+  private
+
+    def cart_items_params
+      params.require(:cart_item).permit(:cart_id, :product_variant_id, :quantity, 
+                     product_variant_attributes: [:size_id])
+    end
+
 end
