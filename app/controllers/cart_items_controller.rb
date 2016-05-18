c8dae16014e3ead65c78969945e946c4fcca048e
Tia <cudoviste>
2016-05-18 19:38:17 +0200

    Bug: Add message when registration is successful

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -21,27 +21,3 @@ class CartItemsController < ApplicationController
     end
 
 end
-
-# class CartItemsController < ApplicationController
-
-#   def create
-#     @product_variant = ProductVariant.find(params[:cart_item][:product_variant_id])
-#     @product = @product_variant.product
-#     @cart_item = CartItem.new(cart_items_params)
-#     if @cart_item.save
-#       flash[:info] = 'Item added to basket'
-#       redirect_to category_product_path(@product.categories.first.id, @product.id )
-#     else
-#       flash[:danger] = 'Quantity missing'
-#       render 'products/show'
-#     end
-#   end
-
-#   private
-
-#     def cart_items_params
-#       params.require(:cart_item).permit(:cart_id, :product_variant_id, :quantity, 
-#                      product_variant_attributes: [ :size_id, :color_id, :id ])
-#     end
-
-# end
