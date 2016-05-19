3e0180f5c0b956f89e01e899f1a886c3a34d2c68
Tia <cudoviste>
2016-05-19 07:34:13 +0200

    Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
+  validates :first_name, :last_name, length: {maximum: 50}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
+  validates :email, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX}
+  
+  validates :password, length: {minimum: 6}
 
   has_one :cart
   has_secure_password
