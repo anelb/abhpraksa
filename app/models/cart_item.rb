d1aa4967489b6f23e102e37baa7c73fffb962daa
Anel <anelbasic@gmail.com>
2016-05-19 13:59:19 +0200

    Add validation - quantity has to be a number

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -5,6 +5,8 @@ class CartItem < ActiveRecord::Base
 
 	validates :cart_id, :product_variant_id, :quantity, presence: true
 
+  validates_numericality_of :quantity, :only_integer => true, :greater_than_or_equal_to => 1
+
   validate :quantity_check
 
   def quantity_check
