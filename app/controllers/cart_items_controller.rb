5f3ae9f48aeafb943f15880c63ba3cabad27c36c
Tia <cudoviste>
2016-05-19 22:29:08 +0200

    Add option to remove product from cart, destroy cart(need improvement)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,9 +1,11 @@
 class CartItemsController < ApplicationController
 
-  before_action :set_product_variant
-  before_action :set_product
 
   def create
+    @product_variant = ProductVariant.find_by(size_id:  params[:product_variant][:size_id], 
+                                              color_id: params[:product_variant][:color_id])
+    @product = @product_variant.product
+
     @cart_item = @product_variant.cart_items.build(cart_items_params)
 
     if @cart_item.save
@@ -14,18 +16,17 @@ class CartItemsController < ApplicationController
     redirect_to category_product_path(@product.categories.first.id, @product.id )
   end
 
+  def destroy
+    cart_item = CartItem.find(params[:cart_item_id])
+    product = cart_item.product_variant.product
+    CartItem.find(params[:cart_item_id]).destroy
+    flash[:info] = 'Product removed from cart'
+    redirect_to category_product_path(product.categories.first.id, product.id )
+  end
+
   private
 
     def cart_items_params
       params.require(:cart_item).permit(:cart_id, :product_variant_id, :quantity)
-    end
-
-    def set_product_variant
-      @product_variant = ProductVariant.find_by(size_id:  params[:product_variant][:size_id], 
-                                              color_id: params[:product_variant][:color_id])
-    end
-
-    def set_product
-      @product = @product_variant.product
     end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,10 @@ class CartsController < ApplicationController
   def show
     @cart = current_cart.cart_items
   end
+
+  def destroy
+  	Cart.find(params[:cart_id]).destroy
+  	flash[:danger] = 'Cart is now deleted'
+  	redirect_to root_path
+  end
 end
