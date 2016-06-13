b4330b40a152d9e7d67eb6c810831e89f302c40c
Anel <anelbasic@gmail.com>
2016-06-13 12:50:15 +0200

    Fix: Added sendgrid settings for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,21 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { host: "abhshopdemo.herokuapp.com" }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
 end
