1c5c17c054f9f4bfbeb79918d49c8955165cc5e0
Tia <cudoviste>
2016-06-27 08:48:56 +0200

    Testing: Category filtes

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -12,21 +12,20 @@ $(document).ready(function() {
     var filter = $(this).val();
     if ( currentParams != '') {
       var filterTitle = filter.split('=')[0].split('?')[1];
-      $.each( currentParams, function(index, param) {
+      var params = currentParams.toString().split('&');
+      $.each( params, function(index, param) {
         if ( param.indexOf(filterTitle) >= 0 ) {
-          var paramsSplited = currentParams.toString();
-          if (paramsSplited.split('=').length == 2) {
-          currentParams.splice(currentParams.indexOf(param), 1);
+          if (params.length == 1) {
+          params.splice(params.indexOf(param), 1);
           window.location = filter + 'One';
           }
           else {
-            var newParams = paramsSplited.split('&')
-            newParams.splice(newParams.indexOf(param), 1);
-            window.location = filter + 'Many' + '&' + newParams;
+            params.splice(params.indexOf(param), 1);
+            window.location = filter + 'Many' + '&' + params;
           };
         }
         else {
-          window.location = filter + '&' + currentParams;
+          window.location = filter + 'newFilter' + '&'  + params;
         };
       });
     }
