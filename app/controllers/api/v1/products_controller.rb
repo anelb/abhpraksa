290e43d6cccf1d5b1f167b1d4152c4ee20be6be7
Anel <anelbasic@gmail.com>
2016-07-12 21:01:59 +0200

    Added error message when product does not exist

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,18 +3,26 @@ class Api::V1::ProductsController < ApiController
   before_action :set_category, only: [:index, :show]
 
   def index
-    if params[:page]
-      @products = @category.products.select(:id, :photo_url).paginate(:page => params[:page], :per_page => 20)
-    else
-      @products = @category.products.select(:id, :photo_url)
+    begin
+      if params[:page]
+        @products = @category.products.select(:id, :photo_url).paginate(:page => params[:page], :per_page => 20)
+      else
+        @products = @category.products.select(:id, :photo_url)
+      end
+      render response: @products
+    rescue
+      #byebug
     end
-    render response: @products
   end
 
 
   def show_product
-    @product = Product.find(params[:product_id])
-    render response: @product.custom_json
+    begin
+      @product = Product.find(params[:product_id])
+      render response: @product.custom_json
+    rescue ActiveRecord::RecordNotFound
+      raise Api::Exceptions::RecordNotFound.new('Product')
+    end
   end
 
   private
