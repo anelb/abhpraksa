30a4febf5dd78ba6b866b6217beb9de13820f102
Anel <anelbasic@gmail.com>
2016-07-19 13:45:21 +0200

    Fixed remember me

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ActiveRecord::Base
 
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_digest, remember_token)
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(attribute, token)
