0591cfbc1adf0784bb09f55b09c102cbc3d6f3d5
Anel <anelbasic@gmail.com>
2016-08-13 15:03:42 +0200

    Fixed dropdowns for brand, category (admin panel)

--- app/assets/javascripts/admin/products.js
+++ app/assets/javascripts/admin/products.js
@@ -1,20 +1,3 @@
-/*$(document).on 'click', 'form .add_fields', (event) ->
-  time = new Date().getTime()
-  regexp = new RegExp($(this).data('id'), 'g')
-  $(this).before($(this).data('fields').replace(regexp, time))
-  event.preventDefault()
-*/
-
-$(document).on('click', 'form .add_fields', function(event) {
-  var regexp, time;
-  time = new Date().getTime();
-  regexp = new RegExp($(this).data('id'), 'g');
-  $(this).before($(this).data('fields').replace(regexp, time));
-  return event.preventDefault();
-});
-
-$(document).on('click', 'form .remove_fields', function(event) {
-  $(this).prev("input[type=hidden]").val('1');
-  $(this).closest('fieldset').hide();
-  return event.preventDefault();
+$(document).ready(function() {
+   $('.selectpicker').selectpicker();
 });
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require toastr
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require admin/products.js
 //= require_tree .
 
